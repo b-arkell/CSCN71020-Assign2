d197afc4f6d84066db15f5b4818a852f19374a2b
b-arkell <broarkell2@gmail.com>
2024-10-16 18:13:05 -0400

    Added unit tests for setLength function

--- BCSRecUnitTests/BCSRecUnitTests.cpp
+++ BCSRecUnitTests/BCSRecUnitTests.cpp
@@ -44,4 +44,46 @@ namespace BCSRecUnitTests
 
 		}
 	};
+
+	TEST_CLASS(setLengthFunction)
+	{
+	public:
+
+		TEST_METHOD(NegativeNumberTest)
+		{
+			// testing negative numbers 
+			int input = -42;
+			int length = 1;
+			
+			setLength(input, &length);
+			Assert::AreEqual(1, length);
+		}
+
+		TEST_METHOD(UpperRangeFunctionality)
+		{
+			// testing upper limit with 100 & 99 for inclusive value 
+			int input = 99;
+			int input2 = 100;
+			int length = 1;
+			int length2 = 1;
+			setLength(input, &length);
+			setLength(input2, &length2);
+			Assert::AreEqual(99, length);
+			Assert::AreEqual(1, length2);
+		}
+		TEST_METHOD(LowerRangeFunctionality)
+		{
+			// testing lower limit with 0 & 1 for inclusive value
+			// test lower limit with 0
+			int input = 0;
+			int length = 1;
+			int input2 = 1;
+			int length2 = 50;
+
+			setLength(input, &length);
+			setLength(input2, &length2);
+			Assert::AreEqual(1, length);
+			Assert::AreEqual(1, length2);
+		}
+	};
 }
